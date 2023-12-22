6262f7f0a75365668121e8e2b5dbddcd190375c0
Sinan Aydın <en.snnaydin@gmail.com>
2023-12-22 16:24:24 +0300

    mongodb connected.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
@@ -7,7 +9,13 @@ app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+mongoose
+    .connect(uri)
+    .then(() => console.log("MongoDB connection established"))
+    .catch((error) => console.log("MongoDB connection failed", error.message));
 
 app.listen(port, (req, res) => {
     console.log(`Server running on port ${port}`);
-})
+});
