dca5da9668d1bed410026904d6a63041952de9da
Sinan Aydın <en.snnaydin@gmail.com>
2023-12-22 19:56:45 +0300

    user model fixed

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String, required: true, minLength: 3, maxLength: 30
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String, required: true, minLength: 3, maxLength: 30
+        },
+        email: {
+            type: String, required: true, minLength: 3, maxLength: 30, unique: true
+        },
+        password: {
+            type: String, required: true, minLength: 3, maxLength: 1024,
+        },
     },
-    email: {
-        type: String, required: true, minLength: 3, maxLength: 30, unique: true
-    },
-    password: {
-        type: String, required: true, minLength: 3, maxLength: 1024,
-    },
-    timestamps: true
-});
+    {
+        timestamps: true,
+    }
+);
 
 const userModel = mongoose.model("User", userSchema);
 
