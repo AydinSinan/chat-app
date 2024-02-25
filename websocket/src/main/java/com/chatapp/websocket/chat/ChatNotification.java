21b54c254448a752daf49183b358adae1eb418e3
sinan <en.snnaydin@gmail.com>
2024-02-25 16:15:29 +0300

    chatmessage configuration add

---
+++ websocket/src/main/java/com/chatapp/websocket/chat/ChatNotification.java
@@ -0,0 +1,17 @@
+package com.chatapp.websocket.chat;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class ChatNotification {
+    private String id;
+    private String senderId;
+    private String recipientId;
+    private String content;
+}
